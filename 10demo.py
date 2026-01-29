02e7d7ac338c76c279c0fbe3d95996e9a5954b74
masynrg <masynrghiorso@gmail.com>
2026-01-28 21:47:21 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -27,3 +27,39 @@ def amean3(a, b, c): return ((a + b + c) / 3)
 def gmean3(a, b, c): return (math.cbrt(a * b * c))
 def hmean3(a, b, c): return (3 / ((1/a) + (1/b) + (1/c)))
 def distance(a, b, x, y): return math.sqrt(((b-a)**2) + ((y-x)**2))
+
+#strings
+
+s = 'hello world'
+print(s, type(s))
+
+#conditionals
+
+def is_even(x):
+    if x % 2 == 0: return True
+    return False
+print(is_even(2))
+print(is_even(3))
+
+#boolean
+
+c = 4 == 4
+print(c)
+print(type(c))
+
+#practice
+
+def is_integer(i):
+    if i % 1 == 0: return True
+    else: return False
+print(is_integer(3))
+print(is_integer(1.3))
+
+def dna_comp(x):
+    if x == 'A': return 'T'
+    if x == 'T': return 'A' 
+    if x == 'G': return 'C'
+    if x == 'C': return 'G' 
+    else:        return None 
+print(dna_comp('G'))
+print(dna_comp('D'))
