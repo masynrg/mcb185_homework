6c71ecaec912a634e2832a3a780a6113aac03721
masynrg <masynrghiorso@gmail.com>
2026-01-28 21:05:00 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -15,6 +15,15 @@ print(math.pi)
 #functions
 
 def pythagoras(a, b):
-    return math.sqrt(a**2 + b**2)
-    
+    return math.sqrt(a**2 + b**2)   
 print(pythagoras(3, 4))
+
+def circle_area(r): return (math.pi * r**2)
+def rectangle_area(w, h): return (w * h)
+
+def f_to_c(f): return ((f - 32) * (5 / 9))
+def mph_to_kph(m): return (m * 1.6)
+def amean3(a, b, c): return ((a + b + c) / 3)
+def gmean3(a, b, c): return (math.cbrt(a * b * c))
+def hmean3(a, b, c): return (3 / ((1/a) + (1/b) + (1/c)))
+def distance(a, b, x, y): return math.sqrt(((b-a)**2) + ((y-x)**2))
