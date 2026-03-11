24ed556dce7ed38334300e59b09d884b41338dcf
masynrg <masynrghiorso@gmail.com>
2026-03-11 07:27:46 -0700

    update

--- 31cdslength.py
+++ 31cdslength.py
@@ -9,3 +9,12 @@ with gzip.open(sys.argv[1], 'rt') as fp:
                 beg = int(words[3])
                 end = int(words[4])
                 print(end - beg + 1)
+
+with gzip.open(sys.argv[1], 'rt') as fp:
+    for line in fp:
+        if line[0] == '#': continue
+        words = line.split()
+        if words[2] != 'CDS': continue
+        beg = int(words[3])
+        end = int(words[4])
+        print(end - beg + 1)
